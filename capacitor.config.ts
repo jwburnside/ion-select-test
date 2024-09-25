cfc30b3d38155330d351a2c8ab1658946b78bf04
Justin Briggs <justinwbriggs@gmail.com>
2024-09-25 14:51:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Select Test',
+  webDir: 'www'
+};
+
+export default config;
